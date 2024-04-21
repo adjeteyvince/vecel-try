08f4019115d606294bc427b3332184b22134a4f1
adjeteyvince <adjeteyspiritus1@gmail.com>
2024-04-21 21:55:41 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Home Page"
+
+@app.route('/about', methods=['GET'])
+def home():
+    return "About KT - RH Media Api"
+
+
+if __name__ == '__main__':
+    app.run()
